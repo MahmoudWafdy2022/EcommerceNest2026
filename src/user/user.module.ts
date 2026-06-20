2a0bf84304f6c2747b3b04438725752afe40986a
Mahmoud Wafdy <mahmoudwafdy18@gmail.com>
2026-06-20 16:51:19 +0300

    uploadProject

---
+++ src/user/user.module.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
