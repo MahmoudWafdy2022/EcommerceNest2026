2baf75e50d4009e5248b0f1d8041f7d664a31b5f
Mahmoud Wafdy <mahmoudwafdy18@gmail.com>
2026-06-21 13:16:44 +0300

    updated user schema with additional fields and validation

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -5,8 +5,72 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop()
+  @Prop({
+    required: true,
+    type: String,
+    minlength: [3, 'Name must be at least 3 characters'],
+    maxlength: [30, 'Name must be at most 50 characters'],
+  })
   name: string;
+
+  @Prop({
+    required: true,
+    unique: true,
+    type: String,
+    // match: [/.+@.+\..+/, 'Please fill a valid email address'],
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    minlength: [6, 'Password must be at least 6 characters'],
+    maxLength: [20, 'Password must be at most 20 characters'],
+  })
+  password: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: ['user', 'admin', 'manager'],
+    default: 'user',
+  })
+  role: string;
+  @Prop({
+    type: String,
+  })
+  avatar: string;
+  @Prop({
+    type: Number,
+  })
+  age: number;
+
+  @Prop({
+    type: String,
+  })
+  phonenumber: string;
+
+  @Prop({
+    type: String,
+  })
+  address: string;
+
+  @Prop({
+    type: Boolean,
+    default: true,
+  })
+  active: boolean;
+
+  @Prop({
+    type: String,
+  })
+  verificationCode: string;
+
+  @Prop({
+    type: String,
+    enum: ['male', 'female'],
+  })
+  gender: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const newUser = this.userModel.create(createUserDto);
+    return newUser;
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find();
   }
 
   findOne(id: number) {
